de99ed84d233cdeb729fad5587812bb3d16b284e
moradalkhatib <muradalkhateb@icloud.com>
2022-11-07 21:04:28 +0300

    new

--- api/trivia.py
+++ api/trivia.py
@@ -17,7 +17,7 @@ class Handler(BaseHTTPRequestHandler):
             r = requests.get(url_http_cat+dic['amount'])
             data = r.json()
             message = ""
-            for i in range(dic['amount']):
+            for i in range(int(dic['amount'])):
                 question = data['results'][i]['question']
                 answer = data['results'][i]['correct_answer']
                 message+=f"{question} The Answer Is: {answer}  \n"
