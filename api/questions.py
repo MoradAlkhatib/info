9896fdddba45eb0f818af5fbed644ddcabc9835b
moradalkhatib <muradalkhateb@icloud.com>
2022-11-08 11:17:09 +0300

    new

--- api/questions.py
+++ api/questions.py
@@ -6,7 +6,7 @@ import requests
 class Handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        url_http_cat = "https://opentdb.com/api.php?amount="
+        
 
         path = self.path
         url_components = parse.urlsplit(path)
@@ -14,6 +14,7 @@ class Handler(BaseHTTPRequestHandler):
         dic = dict(query_string_list)
 
         if "amount" in dic:
+            url_http_cat = "https://opentdb.com/api.php?amount="
             r = requests.get(url_http_cat+dic['amount'])
             data = r.json()
             message = ""
@@ -25,7 +26,19 @@ class Handler(BaseHTTPRequestHandler):
             
             else :
                 message = "We Can Retrieve 50 Question Maximum."
-         
+
+        elif "type" in dic:
+                url_http_cat = "https://opentdb.com/api.php?amount=10&type="
+                r = requests.get(url_http_cat+dic['type'])
+                data = r.json()
+                message += f"{dic['type']}\n"
+                for i in range(10):
+                    question = data['results'][i]['question']
+                    answer = data['results'][i]['correct_answer']
+                    message+=f"{question} The Answer Is: {answer}  \n"
+                
+
+
 
         else :
             message = "hi"
